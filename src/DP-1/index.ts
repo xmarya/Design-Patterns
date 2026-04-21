d2e7c722a6d093dc7a8e5a66e2e5baff35dd4b7d
xmarya <marya_hy@hotmail.com>
2026-04-21 08:45:21 +0300

    finishing the first exercise

---
+++ src/DP-1/index.ts
@@ -0,0 +1,34 @@
+import type { NotificationDTO, NotificationsStrategy } from "./_types/NotificationsStrategy";
+import users from "../shared/_db/users.db.json";
+import { EmailService } from "./Email/EmailService";
+import { SmsService } from "./SMS/SmsService";
+import { InAppNotification } from "./InApp/InAppService";
+
+function sendNotification(data: NotificationDTO, notifier: NotificationsStrategy) {
+  notifier.notify(data);
+}
+
+sendNotification(
+  {
+    type: "signup",
+    to: users["003"],
+    date: new Date(),
+  },
+  new InAppNotification(),
+);
+sendNotification(
+  {
+    type: "new-login",
+    to: users["003"],
+    date: new Date(),
+  },
+  new EmailService(),
+);
+sendNotification(
+  {
+    type: "reset-password",
+    to: users["003"],
+    date: new Date(),
+  },
+  new SmsService(),
+);
