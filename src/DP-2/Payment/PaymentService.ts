a4a9767e699ad8e7265b261918e62bb2e834568c
xmarya <marya_hy@hotmail.com>
2026-05-03 08:15:43 +0300

    fixed the return type of getTransaction method

--- src/DP-2/Payment/PaymentService.ts
+++ src/DP-2/Payment/PaymentService.ts
@@ -1,10 +1,11 @@
 import type { BadRequest } from "../_shared/core/types/responses/BadRequest";
 import type { Failure } from "../_shared/core/types/responses/Failure";
+import type { NotFound } from "../_shared/core/types/responses/NotFound";
 import type { Success } from "../_shared/core/types/responses/Success";
 import type { PaymentData, TransactionData } from "./types";
 
 
 export interface PaymentService {
   makePayment(paymentData: Omit<PaymentData, "id" | "created">): Success<PaymentData> | BadRequest;
-  getTransaction(transactionId: string): Success<TransactionData> | BadRequest | Failure;
+  getTransaction(transactionId: string): Success<TransactionData> | NotFound | Failure;
 }
